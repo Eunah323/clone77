0fc04c4cde6822df4d5b6d5e26e3dc3b2a2ff24b
Eunah323 <unapretty@gmail.com>
2022-04-15 20:03:11 +0900

    Initial commit

---
+++ src/main/java/com/sparta/clone77/Clone77Application.java
@@ -0,0 +1,13 @@
+package com.sparta.clone77;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clone77Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Clone77Application.class, args);
+    }
+
+}
