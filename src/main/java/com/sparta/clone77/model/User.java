61041b00bd990a13911a9e3ac01a1f0351a678dc
HwangSeokJun <unbeatablesj@naver.com>
2022-04-19 18:33:46 +0900

    Feat: 장바구니 수량 변경 및 삭제 기능 추가

--- src/main/java/com/sparta/clone77/model/User.java
+++ src/main/java/com/sparta/clone77/model/User.java
@@ -3,43 +3,32 @@ package com.sparta.clone77.model;
 import com.sparta.clone77.dto.UserRequestDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Formula;
 
 import javax.persistence.*;
-import java.util.List;
 
-@Getter // get 함수를 일괄적으로 만들어줍니다.
-@NoArgsConstructor // 기본 생성자를 만들어줍니다.
-@Entity // DB 테이블 역할을 합니다.
+@Getter
+@Entity
+@NoArgsConstructor
 public class User{
-    // ID가 자동으로 생성 및 증가합니다.
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // 반드시 값을 가지도록 합니다.
     @Column(nullable = false)
     private String username;
 
     @Column(nullable = false)
-    private String name;
-
-    @Column(nullable = false)
     private String password;
 
-    @OneToOne
-    private Cart cart;
+    @Column(nullable = false)
+    private String name;
 
-    @Formula("(select count(1) from orders ordercount where ordercount.user_id = id)")
+    @Column(nullable = false) // nullable 설정은 추후 논의가 필요합니다.
+    private String email;
+
+    @Column(nullable = false)
     private int orderCount;
-
-
-    public User(String username, String name, String password, int orderCount) {
-        this.username = username;
-        this.name = name;
-        this.password = password;
-        this.orderCount = orderCount;
-    }
 
     public User(UserRequestDto requestDto) {
         this.username = requestDto.getUsername();
--- src/main/java/com/sparta/clone77/service/ProductService.java
+++ src/main/java/com/sparta/clone77/service/ProductService.java
@@ -3,18 +3,19 @@ package com.sparta.clone77.service;
 import com.sparta.clone77.dto.ProductResponseDto;
 import com.sparta.clone77.model.Product;
 import com.sparta.clone77.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService {
 
-    @Autowired
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
+    // 상품 조회
     public List<ProductResponseDto> getProducts(String tab){
 
         List<Product> products = productRepository.findAllByCategory(tab);
