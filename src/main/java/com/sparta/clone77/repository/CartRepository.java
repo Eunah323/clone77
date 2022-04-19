cadf422c88b6d194d3ac245fe291538072426972
HwangSeokJun <unbeatablesj@naver.com>
2022-04-19 21:42:39 +0900

    Chore: 쿼리 삭제

--- src/main/java/com/sparta/clone77/repository/CartRepository.java
+++ src/main/java/com/sparta/clone77/repository/CartRepository.java
@@ -9,7 +9,4 @@ import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
     Optional<Cart> findByUserId(Long userId);
-
-    @Query("select distinct c, i from Cart c, CartItem i left join fetch c.cartItems where c.userId = :userId and i.cart = c")
-    Cart findCartFetchJoin(@Param("userID") Long userId);
 }
